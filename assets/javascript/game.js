423c760eed446b5cd08ec48d09ab105a7e634d18
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-11 20:29:10 -0400

    basis of game before scoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,27 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//computer chooses letter
-var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
-console.log(letter);
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+var left = 8;
 
-//person guesses
+//person guesses by typing letter
+document.onkeyup = function(event) {
+    var guess = event.key;
+    console.log(guess);
 
-if (guess===letter) {
-    console.log(you win);
-}
+//computer chooses letter after letter typed
+    var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
+    console.log(letter);
 
-else {
-    //add one to guess
-    //subtract 1 from guesses left
+    if (guess===letter) {
+        console.log("you win");
+    }
+
+    else {
+        console.log("you lose");
+        //add one to guess
+        //subtract 1 from guesses left
+    }
+
 }
