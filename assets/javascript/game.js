fdd4d194b26686da7eb31566c4bc0a82e520f867
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-11 20:56:21 -0400

    displays values in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var wins = 0;
-var losses = 0;
-var guesses = 0;
-var left = 8;
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+var guesses = document.getElementById("guesses");
+var left = document.getElementById("left");
 
 //person guesses by typing letter
 document.onkeyup = function(event) {
@@ -16,12 +16,22 @@ document.onkeyup = function(event) {
 
     if (guess===letter) {
         console.log("you win");
+        wins++;
+        console.log("wins: " + wins);
     }
 
     else {
         console.log("you lose");
-        //add one to guess
+        losses++;
+        console.log("losses: " + losses);
+        guesses++;
+        console.log("guesses: " + guesses);
         //subtract 1 from guesses left
     }
 
+    //update the stats in HTML
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("guesses").innerHTML = guesses;
+    document.getElementById("left").innerHTML = left;
 }
