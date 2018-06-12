b1145c5dae72e3eabd2cf5b2e0401b625588bfdb
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-12 13:17:12 -0400

    fixed variables at beginning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = document.getElementById("wins");
+var wins = 0;
 var losses = document.getElementById("losses");
+var losses = 0;
 var guesses = document.getElementById("guesses");
 var left = document.getElementById("left");
 var left = 8;
@@ -13,34 +15,34 @@ console.log("computer choice: " + letter);
 
 //person guesses by typing letter
 document.onkeyup = function(event) {
-    var guess = event.key;
+    guess = event.key;
     console.log("player guess: " + guess);
+    // var guesses = guess;
 
 
     if (guess===letter) {
         console.log("you win");
         wins++;
-        losses += 0;
         console.log("wins: " + wins);
-        var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
-        console.log("computer choice: " + letter);
+        left = 8;
+        // var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
+        // console.log("computer choice: " + letter);
     }
 
     else if (left > 0) {
-        wins += 0;
-        losses += 0;
         (left -= 1);;
         console.log("guesses: " + guesses);
         //subtract 1 from guesses left
-        guesses += event.key + " ";
+        guesses += guess + ", ";
     }
 
     else {
         console.log("you lose");
         losses++;
         console.log("losses: " + losses);
-        var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
-        console.log("computer choice: " + letter);
+        left = 8;
+        // var letter = alphabet[Math.floor(Math.random() * (alphabet.length))];
+        // console.log("computer choice: " + letter);
     }
 
     //update the stats in HTML
